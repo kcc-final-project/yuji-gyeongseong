0961492e0e6ed7dd3b179d015cb9b9626ef9bade
gus-123 <hyunmin1711@gmail.com>
2024-10-08 09:19:39 +0900

    feat: evaluation-table modify [#5]

---
+++ src/main/java/com/yujigyeongseong/api/domain/work_lounge/controller/WorkLoungeController.java
@@ -0,0 +1,28 @@
+package com.yujigyeongseong.api.domain.work_lounge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/work-lounge")
+@Controller
+public class WorkLoungeController {
+
+    @GetMapping
+    public String getWorkLoungePage() {
+        return "work-lounge/work-lounge";
+    }
+
+    @GetMapping("/evaluation-table")
+    public String getWorkLoungeEvaluationTablePage() {
+        return "work-lounge/evaluation-table";
+    }
+
+    @GetMapping("/evaluation-paper")
+    public String getWorkLoungeEvaluationPaperPage() {
+        return "work-lounge/evaluation-paper";
+    }
+
+
+
+}
