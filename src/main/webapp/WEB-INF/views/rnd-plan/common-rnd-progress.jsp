bb95cc4095bb3ea51011c84994d2a6bae8d9daca
Cheol-Cheol <dream174822@gmail.com>
2024-10-09 19:31:36 +0900

    feat: 프로그래스 바 스크롤 시 상단 고정 [#2]

--- src/main/webapp/WEB-INF/views/rnd-plan/common-rnd-progress.jsp
+++ src/main/webapp/WEB-INF/views/rnd-plan/common-rnd-progress.jsp
@@ -17,27 +17,27 @@
         </div>
 
         <div class="step-container d-flex justify-content-between">
-            <div class="step-group" onclick="displayStep(1)">
+            <div class="step-group" onclick="changeStep(1)">
                 <div class="step-circle">1</div>
                 <span>기본정보</span>
             </div>
-            <div class="step-group" onclick="displayStep(2)">
+            <div class="step-group" onclick="changeStep(2)">
                 <div class="step-circle">2</div>
                 <span>과제요약</span>
             </div>
-            <div class="step-group" onclick="displayStep(3)">
+            <div class="step-group" onclick="changeStep(3)">
                 <div class="step-circle">3</div>
                 <span>연구기관</span>
             </div>
-            <div class="step-group" onclick="displayStep(4)">
+            <div class="step-group" onclick="changeStep(4)">
                 <div class="step-circle">4</div>
                 <span>연구개발비</span>
             </div>
-            <div class="step-group" onclick="displayStep(5)">
+            <div class="step-group" onclick="changeStep(5)">
                 <div class="step-circle">5</div>
                 <span>첨부파일</span>
             </div>
-            <div class="step-group" onclick="displayStep(6)">
+            <div class="step-group" onclick="changeStep(6)">
                 <div class="step-circle">6</div>
                 <span>최종제출</span>
             </div>
--- src/main/webapp/resources/js/rnd-plan/common-rnd-progress.js
+++ src/main/webapp/resources/js/rnd-plan/common-rnd-progress.js
@@ -1,28 +1,87 @@
 const MIN_STEP = 1;
 const MAX_STEP = 6;
+const ANIMATION_TIMEOUT = 500;
+const STEP_PATHS = [
+  "/basic", // 기본정보
+  "/task-summary", // 과제요약
+  "/rsrch-institution", // 연구기관
+  "/rnd-expenses", // 연구개발비
+  "/additional-file", // 첨부파일
+  "/final-submit", // 최종제출
+];
 
 let currentStep = 1;
 
-// 현재 단계에 따른 진행률 변경
-const displayStep = (stepNumber) => {
+$(function () {
+  initializeProgressBar();
+  setupEventHandlers();
+  handleScrollEffect();
+});
+
+// 이벤트 핸들러
+function setupEventHandlers() {
+  $(".next-step").on("click", moveToNextStep);
+  $(".prev-step").on("click", moveToPrevStep);
+}
+
+// 다음 단계로 이동
+function moveToNextStep() {
+  if (currentStep < MAX_STEP) {
+    $(".step-" + currentStep).addClass(
+      "animate__animated animate__fadeOutLeft",
+    );
+    currentStep++;
+    setTimeout(function () {
+      $(".step").removeClass("animate__animated animate__fadeOutLeft").hide();
+      $(".step-" + currentStep)
+        .show()
+        .addClass("animate__animated animate__fadeInRight");
+      updateProgressBar();
+    }, ANIMATION_TIMEOUT);
+  }
+}
+
+// 이전 단계로 이동
+function moveToPrevStep() {
+  if (currentStep > MIN_STEP) {
+    $(".step-" + currentStep).addClass(
+      "animate__animated animate__fadeOutRight",
+    );
+    currentStep--;
+    setTimeout(function () {
+      $(".step").removeClass("animate__animated animate__fadeOutRight").hide();
+      $(".step-" + currentStep)
+        .show()
+        .addClass("animate__animated animate__fadeInLeft");
+      updateProgressBar();
+    }, ANIMATION_TIMEOUT);
+  }
+}
+
+// 프로그래스 바 초기화
+function initializeProgressBar() {
+  updateProgressBar();
+  loadStepContent(currentStep);
+}
+
+// 이동할 단계에 대한 이벤트 처리
+function changeStep(stepNumber) {
   if (MIN_STEP <= stepNumber && stepNumber <= MAX_STEP) {
     currentStep = stepNumber;
     updateProgressBar();
-
-    const path = switchPathFrom(currentStep);
-    loadStepContent(path);
+    loadStepContent(currentStep);
   }
-};
+}
 
 // 프로그래스 바 진행률 변경
-const updateProgressBar = () => {
+function updateProgressBar() {
   const progressPercentage = ((currentStep - 1) / (MAX_STEP - 1)) * 100;
   $(".progress-bar").css("width", progressPercentage + "%");
   updateStepStyles();
-};
+}
 
-// 진행률 스타일 변경
-const updateStepStyles = () => {
+// 프로그래스 바 진행률 스타일 변경
+function updateStepStyles() {
   $(".step-group").each(function (index) {
     if (index + 1 === currentStep) {
       $(this).find(".step-circle").addClass("active-circle");
@@ -31,77 +90,32 @@ const updateStepStyles = () => {
       $(this).find("span").removeClass("active-step");
     }
   });
-};
-
-// 요청 경로 반환
-const switchPathFrom = (currentStep) => {
-  switch (currentStep) {
-    case 1:
-      return "/basic";
-    case 2:
-      return "/task-summary";
-    case 3:
-      return "/rsrch-institution";
-    case 4:
-      return "/rnd-expenses";
-    case 5:
-      return "/additional-file";
-    case 6:
-      return "/final-submit";
-  }
-};
+}
 
 // 페이지 컴포넌트 AJAX 요청
-const loadStepContent = (path) => {
+function loadStepContent(currentStep) {
+  const path = STEP_PATHS[currentStep - 1];
+
   $.ajax({
     url: "/rnd-plan" + path,
     type: "GET",
-    success: function (res) {
+    success: (res) => {
       $("#step-content").html(res);
     },
-    error: function (err) {
+    error: (err) => {
       console.log("[loadStepContent()] " + err.statusText + " - " + err.status);
     },
   });
-};
+}
 
-$(document).ready(function () {
-  updateProgressBar();
+// 스크롤 시 프로그래스 바 상단 고정 및 그림자 효과 부여
+function handleScrollEffect() {
+  const headerHeight = $(".common-header").outerHeight();
 
-  const path = switchPathFrom(currentStep);
-  loadStepContent(path);
-
-  $(".next-step").click(function () {
-    if (currentStep < MAX_STEP) {
-      $(".step-" + currentStep).addClass(
-        "animate__animated animate__fadeOutLeft",
-      );
-      currentStep++;
-      setTimeout(function () {
-        $(".step").removeClass("animate__animated animate__fadeOutLeft").hide();
-        $(".step-" + currentStep)
-          .show()
-          .addClass("animate__animated animate__fadeInRight");
-        updateProgressBar();
-      }, 500);
-    }
+  $(window).scroll(() => {
+    $(".multi-step-progress-bar").toggleClass(
+      "bar-shadow",
+      $(window).scrollTop() > headerHeight,
+    );
   });
-
-  $(".prev-step").click(function () {
-    if (currentStep > MIN_STEP) {
-      $(".step-" + currentStep).addClass(
-        "animate__animated animate__fadeOutRight",
-      );
-      currentStep--;
-      setTimeout(function () {
-        $(".step")
-          .removeClass("animate__animated animate__fadeOutRight")
-          .hide();
-        $(".step-" + currentStep)
-          .show()
-          .addClass("animate__animated animate__fadeInLeft");
-        updateProgressBar();
-      }, 500);
-    }
-  });
-});
+}
