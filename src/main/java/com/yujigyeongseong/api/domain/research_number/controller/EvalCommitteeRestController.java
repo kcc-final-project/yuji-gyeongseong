4cfe055ad7afd436fb5032bb2ae16cda30c8bc03
dnscodnjs <yun9869@naver.com>
2024-10-16 14:49:28 +0900

    feat : 더미 데이터 생성 및 DB 조회 구현 [#47]

---
+++ src/main/java/com/yujigyeongseong/api/domain/research_number/controller/EvalCommitteeRestController.java
@@ -0,0 +1,21 @@
+package com.yujigyeongseong.api.domain.research_number.controller;
+
+import com.yujigyeongseong.api.domain.research_number.dto.AcadAbility;
+import com.yujigyeongseong.api.domain.research_number.dto.Career;
+import com.yujigyeongseong.api.domain.research_number.service.EvalCommitteeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/research_number")
+@RestController
+@RequiredArgsConstructor
+public class EvalCommitteeRestController {
+
+}
