33edc21d0a40573b247d327062c109ebbb518a26
poppyee <sanghk32@naver.com>
2024-10-21 15:17:42 +0900

    feat: 통합 공고 페이지 접수상태 조회 AJAX 구현 [#60]

--- src/main/java/com/yujigyeongseong/api/domain/announcement/dao/AnnouncementMapper.java
+++ src/main/java/com/yujigyeongseong/api/domain/announcement/dao/AnnouncementMapper.java
@@ -1,6 +1,11 @@
 package com.yujigyeongseong.api.domain.announcement.dao;
 
 import com.yujigyeongseong.api.domain.announcement.dto.Announcement;
+import com.yujigyeongseong.api.domain.announcement.dto.SubAnnouncement;
+import com.yujigyeongseong.api.domain.announcement.dto.SubAnnouncementApply;
+import com.yujigyeongseong.api.domain.announcement.dto.request.AnnouncementFilterRequest;
+import com.yujigyeongseong.api.domain.announcement.dto.request.SubAnnouncementDetailRequest;
+import com.yujigyeongseong.api.domain.announcement.dto.request.SubAnnouncementRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -13,4 +18,12 @@ public interface AnnouncementMapper {
     List<Announcement> selectExpectedAnnouncementList();
 
     List<Announcement> selectDeadLineAnnouncementList();
+
+    SubAnnouncementRequest selectAnnouncementByTotalAnnNo(String totalAnnNo);
+
+    SubAnnouncementDetailRequest selectSubAnnouncementBySubAnnNo(Long subAnnoNo);
+
+    SubAnnouncementApply selectSubAnnouncementByAnnNo(String annNo);
+
+    List<Announcement> searchAnnouncement(AnnouncementFilterRequest announcementFilterRequest);
 }
