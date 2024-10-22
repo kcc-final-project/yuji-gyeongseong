be357090c4fb4a90bfc1b55b94cf1698b90fcd95
Cheol-Cheol <dream174822@gmail.com>
2024-10-22 09:11:52 +0900

    chore: clear git cache

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM eclipse-temurin:17-jdk-alpine
-COPY ./target/*SNAPSHOT.jar project.jar
-ENTRYPOINT ["java", "-jar", "project.jar"]
--- src/main/webapp/resources/js/work-lounge/eval-committee-compose.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("#composeButton").on("click", function () {
-    $("#committee-standby").hide();
-    $("#committee-info").show();
-    $(".committee-summary").show();
-  });
-});
