3728a3651e5d8e7d030ff036a2e7b1e95e314045
dnscodnjs <yun9869@naver.com>
2024-10-23 12:22:47 +0900

    feat : 후보단 신청 페이지 ajax 구현 [#57]

--- src/main/webapp/resources/js/research-number/eval-committee-register.js
+++ src/main/webapp/resources/js/research-number/eval-committee-register.js
@@ -1,7 +1,7 @@
 $("#registerButton").on("click", function () {
   Swal.fire({
-    title: "후보단 등록을 하시겠습니까 ?",
-    text: "정보가 올바른지 확인해주세요 !",
+    title: "후보단 등록을 하시겠습니까?",
+    text: "정보가 올바른지 확인해주세요!",
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#2e406a",
@@ -9,12 +9,35 @@ $("#registerButton").on("click", function () {
     confirmButtonText: "신청",
     cancelButtonText: "취소",
   }).then((result) => {
+
+    // 로그인 회원의 번호
+    let memberId = 1;
+
+    let EvalNotiRequest ={
+      content: "평가위원 후보 등록이 되었습니다.",
+      notiType: "평가위원",
+      dataCategory: "service",
+      memNo: memberId
+    };
+
     if (result.isConfirmed) {
-      Swal.fire({
-        title: "신청되었습니다!",
-        text: "평가위원단 후보 등록에 성공하셨습니다.",
-        icon: "success",
-        confirmButtonColor: "#2e406a",
+      $.ajax({
+        url: "/api/v1/research_number/register/eval/" + memberId,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(EvalNotiRequest),
+        success: function(response) {
+          Swal.fire({
+            title: "신청되었습니다!",
+            text: "평가위원단 후보 등록에 성공하셨습니다.",
+            icon: "success",
+            confirmButtonColor: "#2e406a",
+          });
+        },
+        error:function (error){
+          alert("후보단 등록 실패")
+          console.log(error, "error");
+        }
       });
     }
   });
