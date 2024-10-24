85d40bcb297bdb7b368f783bd256eaff358f46c1
gus-123 <hyunmin1711@gmail.com>
2024-10-25 00:11:50 +0900

    feat: register-list Back-End 기능 구현 [#58]

---
+++ src/main/java/com/yujigyeongseong/api/domain/work_lounge/dao/RegisterListMapper.java
@@ -0,0 +1,15 @@
+package com.yujigyeongseong.api.domain.work_lounge.dao;
+
+import com.yujigyeongseong.api.domain.work_lounge.dto.RegisterListDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface RegisterListMapper {
+
+    List<RegisterListDTO> selectRegisterList(@Param("memNo") Integer memNo);
+
+    List<RegisterListDTO> selectCompleteList(int memNo);
+}
