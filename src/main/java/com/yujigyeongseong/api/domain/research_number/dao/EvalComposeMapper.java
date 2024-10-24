e45af44b7eac39822ff3435bf1d7a147e189334c
dnscodnjs <yun9869@naver.com>
2024-10-24 19:39:39 +0900

    feature : 알림에 따른 구성 화면 변경  [#70]

--- src/main/java/com/yujigyeongseong/api/domain/research_number/dao/EvalComposeMapper.java
+++ src/main/java/com/yujigyeongseong/api/domain/research_number/dao/EvalComposeMapper.java
@@ -3,19 +3,34 @@ package com.yujigyeongseong.api.domain.research_number.dao;
 import com.yujigyeongseong.api.domain.research_number.dto.EvalCommittee;
 import com.yujigyeongseong.api.domain.research_number.dto.EvaluationMember;
 import com.yujigyeongseong.api.domain.research_number.dto.Member;
-import com.yujigyeongseong.api.domain.research_number.dto.SubAnnouncement;
+import com.yujigyeongseong.api.domain.research_number.dto.SubAnnounce;
+import com.yujigyeongseong.api.domain.research_number.dto.request.EvalMemberRequest;
 import com.yujigyeongseong.api.domain.research_number.dto.request.MemberDetails;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface EvalComposeMapper {
-    SubAnnouncement selectSubAnnounceById(Long id);
+    SubAnnounce selectSubAnnounceById(Long id);
+
     EvalCommittee selectOneEvalCommitteeById(Long id);
+
     List<EvalCommittee> selectAllEvalCommitteeById(Long id);
+
     int selectRndPlanCntById(Long id);
+
     List<EvaluationMember> selectEvaluationMembersById(Long id);
+
     Member selectOneMemberById(Long id);
+
     MemberDetails selectOneMemberDetailWithInstitution(Long id);
+
+    int udpateEvaluationMemberByNotiContentNo(
+            @Param("memberId") Long memberId,
+            @Param("notiContentNo") Long notiContentNo,
+            @Param("evalMemberRequest") EvalMemberRequest evalMemberRequest);
+
+    int updateNotiByNotificationNo(Long id);
 }
