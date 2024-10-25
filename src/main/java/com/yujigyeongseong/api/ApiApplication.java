ed22d31741100db31b6f5ca35aaed318f4172bb3
Cheol-Cheol <dream174822@gmail.com>
2024-10-25 09:12:10 +0900

    deploy: 배포 관련 설정 수정

--- src/main/java/com/yujigyeongseong/api/ApiApplication.java
+++ src/main/java/com/yujigyeongseong/api/ApiApplication.java
@@ -2,9 +2,17 @@ package com.yujigyeongseong.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
