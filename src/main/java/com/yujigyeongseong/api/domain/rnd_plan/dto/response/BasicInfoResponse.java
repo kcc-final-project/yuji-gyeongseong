9d06c876b2396abbcc6d96de5786f3ac800f1980
dnscodnjs <yun9869@naver.com>
2024-10-31 10:16:40 +0900

    feature : 계획서 기본정보 페이지 수정 [#86]

--- src/main/java/com/yujigyeongseong/api/domain/rnd_plan/dto/response/BasicInfoResponse.java
+++ src/main/java/com/yujigyeongseong/api/domain/rnd_plan/dto/response/BasicInfoResponse.java
@@ -1,21 +1,32 @@
 package com.yujigyeongseong.api.domain.rnd_plan.dto.response;
 
+import com.yujigyeongseong.api.domain.member.dto.Institution;
+import com.yujigyeongseong.api.domain.research_number.dto.Member;
 import com.yujigyeongseong.api.domain.rnd_plan.dto.RndField;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class BasicInfoResponse {
 
     private final String taskName;
     private final String rndTaskNo;
+    private final Long memNo;
     private List<RndField> rndFields;
+    private Member member;
+    private Institution institution;
 
     public void assignRndFields(List<RndField> rndFields) {
         this.rndFields = rndFields;
     }
+
+    public void assignMember(Member member) {this.member = member;}
+
+    public void assignInstitution(Institution institution) {this.institution = institution;}
 
 }
