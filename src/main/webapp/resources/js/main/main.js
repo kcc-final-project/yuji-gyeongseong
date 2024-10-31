5b3391993549bdfd8846a7bccdb1eeab5176b190
poppyee <sanghk32@naver.com>
2024-10-31 10:04:02 +0900

    feature : 메인페이지 API 구현

---
+++ src/main/webapp/resources/js/main/main.js
@@ -0,0 +1,61 @@
+$(function () {
+    loadAnnouncements();
+
+    function loadAnnouncements() {
+        $.ajax({
+            url: '/anno/accepting',
+            method: 'GET',
+            success: function (data) {
+                renderAnnouncements(data);
+            },
+            error: function () {
+                alert('공고 목록을 불러오는 데 실패했습니다.');
+            }
+        });
+    }
+
+    function renderAnnouncements(announcements) {
+
+        for (let i = 0; i < Math.min(9, announcements.length); i++) {
+            var announcementRequest = announcements[i];
+            var announcementHtml = `
+        <div class="anno-content-area">
+            <div>
+                <span class="common-span-style competent_ministry">
+                    ${announcementRequest.competentMinistry}
+                </span>
+                <em class="spec-style spec_institution" data-annNo = ${announcementRequest.annNo} >
+                    ${announcementRequest.specInstitution}
+                </em>
+            </div>
+            <p class="total-title">
+                ${announcementRequest.totalTitle}
+            </p>
+            <small class="state">
+                ${announcementRequest.status}
+            </small>
+        </div>
+        `;
+            $('.anno-contents-body').append(announcementHtml);
+        }
+
+        $('.anno-content-area').click(function () {
+            var AnnNo = $(this).find('.spec-style').attr('data-annNo');
+            window.location.href = `/anno/detail/${AnnNo}`;
+        })
+
+    }
+
+    $('#accepting').addClass('active');
+
+    $('.common-anno-btn').click(() => {
+        $('.common-anno-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.work-lounge').click(function () {
+        console.log()
+        window.location.href = '/work-lounge/business-timeline';
+    })
+
+})
