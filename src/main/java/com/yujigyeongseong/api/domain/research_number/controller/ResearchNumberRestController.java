391ab3c6a67ed6d6f4669623a51cabed070e0c2a
dnscodnjs <yun9869@naver.com>
2024-11-01 10:54:01 +0900

    feature : 컨트롤러 수정 [#93]

--- src/main/java/com/yujigyeongseong/api/domain/research_number/controller/ResearchNumberRestController.java
+++ src/main/java/com/yujigyeongseong/api/domain/research_number/controller/ResearchNumberRestController.java
@@ -6,6 +6,7 @@ import com.yujigyeongseong.api.domain.research_number.service.EvalCommitteeServi
 import com.yujigyeongseong.api.domain.research_number.service.EvalComposeService;
 import com.yujigyeongseong.api.global.auth.PrincipalDetail;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api/v1/research-number")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ResearchNumberRestController {
 
     private final EvalCommitteeService evalCommitteeService;
@@ -28,6 +30,7 @@ public class ResearchNumberRestController {
         // 세션 연결
         Long id = 25L;
 
+        log.info("djslkfjdlsk");
 
         try {
             evalCommitteeService.setCareersByMemberId(id, submitRequest.getCareerInfos());
@@ -44,9 +47,8 @@ public class ResearchNumberRestController {
     }
 
     @PostMapping("/register/eval/{memberId}")
-    public ResponseEntity<?> registerEvalInformation(@RequestBody EvalNotiRequest evalNotiRequest, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+    public ResponseEntity<?> registerEvalInformation(@RequestBody EvalNotiRequest evalNotiRequest) {
         try {
-            evalNotiRequest.setMemNo(principalDetail.getId());
             evalCommitteeService.setEvalNotiByMemberId(evalNotiRequest);
             return ResponseEntity.ok("후보단 신청 완료");
         } catch (Exception e) {
--- src/main/java/com/yujigyeongseong/api/domain/research_number/dto/EvaluationMember.java
+++ src/main/java/com/yujigyeongseong/api/domain/research_number/dto/EvaluationMember.java
@@ -2,11 +2,13 @@ package com.yujigyeongseong.api.domain.research_number.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 import java.sql.Timestamp;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class EvaluationMember {
     private final Long evalCommitteeNo;
     private final Long memNo;
